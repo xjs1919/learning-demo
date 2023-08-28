7dc9ae7c2a1e289866d83cb9b1f60f5bf95a84a3
xjs1919 <xjs@163.com>
2023-08-28 16:26:34 +0800

    feign-compress

---
+++ feign-compress-demo/order-service/src/main/java/com/github/xjs/feign/UserClient.java
@@ -0,0 +1,12 @@
+package com.github.xjs.feign;
+
+import com.github.xjs.pojo.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "user-service", url = "http://localhost:8081")
+public interface UserClient {
+    @GetMapping("/user/{id}")
+    public User queryById(@PathVariable("id") Long id);
+}
---
+++ feign-compress-demo/user-service/src/main/java/com/github/xjs/web/UserController.java
@@ -0,0 +1,21 @@
+package com.github.xjs.web;
+
+import com.github.xjs.pojo.User;
+import com.github.xjs.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id) {
+        return userService.queryById(id);
+    }
+}
