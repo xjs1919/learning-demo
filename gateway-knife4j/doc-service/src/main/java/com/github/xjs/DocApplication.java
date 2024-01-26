67a0a6e3aef77ddceea32e2717f1c1c27e089733
xjs1919 <xjs@163.com>
2024-01-26 14:13:57 +0800

    文档聚合

--- gateway-knife4j/doc-service/src/main/java/com/github/xjs/DocApplication.java
+++ gateway-knife4j/doc-service/src/main/java/com/github/xjs/DocApplication.java
@@ -3,16 +3,11 @@ package com.github.xjs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 参考：
- * http://www.codebaoku.com/it-java/it-java-260248.html
- * https://juejin.cn/post/6854573219916201997
- * */
 @SpringBootApplication
-public class GatewayApplication {
+public class DocApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(DocApplication.class, args);
     }
 
 }
