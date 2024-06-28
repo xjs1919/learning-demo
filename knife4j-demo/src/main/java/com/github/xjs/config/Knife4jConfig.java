f009ae9e76632be0e20ed9e3d237487d82fa3c3d
xjs1919 <xjs@163.com>
2024-06-28 13:12:24 +0800

    knife4j

--- knife4j-demo/src/main/java/com/github/xjs/config/Knife4jConfig.java
+++ knife4j-demo/src/main/java/com/github/xjs/config/Knife4jConfig.java
@@ -1,15 +1,14 @@
 package com.github.xjs.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-@Configuration
+// 方式1
+// @Configuration
 public class Knife4jConfig {
     @Bean
     public Docket docket() {
