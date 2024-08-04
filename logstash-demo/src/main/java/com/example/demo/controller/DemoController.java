337d09b11e23376762ada1537294527dff7fde47
xjs1919 <xjs@163.com>
2024-08-04 13:22:58 +0800

    logstash-demo

---
+++ logstash-demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ThinkPad
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    private static Logger log = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("/hello")
+    public String getById() {
+        log.info("this is a log");
+        return "world";
+    }
+
+    @GetMapping("/exception")
+    public String exception() {
+        try{
+            int a = 1/0;
+        }catch(Exception e){
+            log.error(e.getMessage(), e);
+        }
+        return "world";
+    }
+
+}
